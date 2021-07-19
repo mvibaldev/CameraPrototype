bd03652d42ab875e4b4c1fca8c8cdf23af2df0a3
mvibaldev <mvibaldev1@gmail.com>
2021-07-19 04:59:31 -0500

    agregando la capa de la vista

--- CameraScanner/app/src/main/java/mx/ine/institucional/movil/camerascanner/view/ui/activities/MainActivity.kt
+++ CameraScanner/app/src/main/java/mx/ine/institucional/movil/camerascanner/view/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package mx.ine.institucional.movil.camerascanner
+package mx.ine.institucional.movil.camerascanner.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mx.ine.institucional.movil.camerascanner.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
